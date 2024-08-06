4a73875cd2b2ef95841a2ab978d6130a488fa9e1
waghb <163538951+waghb@users.noreply.github.com>
2024-08-06 23:21:23 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+let v1=document.querySelector('.vehicle1');
+v1.addEventListener('click',function(){
+    window.location.href='sub_page.html';
+})
+let v2=document.querySelector('.vehicle2');
+v2.addEventListener('click',function(){
+    window.location.href='sub_car.html';
+})
